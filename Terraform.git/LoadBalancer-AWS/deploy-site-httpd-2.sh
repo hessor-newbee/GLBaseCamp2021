d75fd5888942e0e740953493b056ee717a27363b
hessor-newbee <56387098+hessor-newbee@users.noreply.github.com>
2021-06-17 15:44:28 +0300

    Update deploy-site-httpd-2.sh

--- Terraform.git/LoadBalancer-AWS/deploy-site-httpd-2.sh
+++ Terraform.git/LoadBalancer-AWS/deploy-site-httpd-2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo yum install httpd -y
 sudo echo "<html><body><h1>Hello from server 2!</h1></body></html>" > /var/www/html/index.html
-sudo systemctl start httpd
 sudo systemctl restart httpd
